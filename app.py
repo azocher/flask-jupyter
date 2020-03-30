0f5350f0f0614af10d24b32183f0ebdc8e0d8a60
azocher <anna.zocher@gmail.com>
2020-03-29 20:11:04 -0700

    example pandas passing to frontend

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, render_template
+# import csv to read data file
 import csv
+# import pandas to interpret data file
+import pandas as pd
 
 app = Flask(__name__)
 
 # add url or reference to data file
 data_file = "./data/names.csv"
 
+# import scripts from jupyter notebook file
+baby_names = pd.read_csv(data_file)
+
 @app.route('/', methods=['GET', 'POST'])
 def names():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        return names
+        return render_template('return.html', tables=baby_names['Gender'].value_counts())
