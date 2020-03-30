9668042895733bd208e47f70fa540123c0c6e72e
azocher <anna.zocher@gmail.com>
2020-03-29 19:28:54 -0700

    added data file

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template
+import csv
 
 app = Flask(__name__)
+
+# add url or reference to data file
+data_file = "./data/names.csv"
 
 @app.route('/', methods=['GET', 'POST'])
 def names():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        return request.form['name']
+        return names
